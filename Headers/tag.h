e79e6c7bf96d1ef4d0993f6360c0dafa88fe3cf5
Chiara Protopapa <tino.pino1207@gmail.com>
2026-06-01 17:58:00 +0200

    implement tag

--- Headers/tag.h
+++ Headers/tag.h
@@ -6,20 +6,22 @@
 
 class tag {
 private:
-    unsigned int id;
     std::string name;
-    bool fix;
+    bool fix; //fix or temporary tag
     QColor color;
 
 public:
-    tag() {}; // da ridefinire!!
-    ~tag() {};
+    tag() {const std:: string& name const Qcolor& color, bool fix=false}; // default: tempoary tag
+    ~tag() = default;
+
     std::string getName() const;
     QColor getColor() const;
     bool getFix() const;
-    void setName(const std::string&);
-    void setFix(const bool&);
-    void setColor(const QColor&);
+
+    void setName(const std::string& name);
+    void setColor(const QColor& color);
+    void setFix(const bool& fix);
+    bool operator == (const tag& t) const; //use to filter by tag
 
 };
 
--- Sources/tag.cpp
+++ Sources/tag.cpp
@@ -1,8 +1,16 @@
 #include "Headers/tag.h"
 
+//constructor
+tag::tag(const std::string& name, const QColor& color, bool fix)
+
 std::string tag::getName() const {return name;}
 QColor tag::getColor() const {return color;}
 bool tag::getFix() const {return fix;}
+
 void tag::setName(const std::string& n) {name=n;}
-void setFix(const bool& b) { fix=b; }
 void setColor(const QColor& c) { color=c;}
+void setFix(const bool& b) { fix=b; }
+
+bool tag::operator==(const tag& t) const {
+    return name == t.getName();
+}

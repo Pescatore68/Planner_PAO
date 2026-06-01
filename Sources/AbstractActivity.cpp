5bb411f459963cde58a1dcd143379244cd2ffe9f
Pescatore68 <chensofia2005@gmail.com>
2026-06-01 18:38:55 +0200

    fix AbsActivity

--- Sources/AbstractActivity.cpp
+++ Sources/AbstractActivity.cpp
@@ -1,3 +1,16 @@
 #include "Headers/AbstractActivity.h"
 
-    AbstractActivity(const string&, const string&) : id(
+AbstractActivity::AbstractActivity(const string& n, const string& d) : name(n), description(d), t() {};
+AbstractActivity::AbstractActivity(const string& n, const string& d, const tag& t) : name(n), description(d), t(t) {};
+
+string AbstractActivity::getName() const { return name; }
+string AbstractActivity::getDesc() const { return description; }
+tag AbstractActivity::getTag() const { return t; }
+
+void AbstractActivity::setName(const string& n) { name = n; }
+void AbstractActivity::setDesc(const string& d) { description = d; }
+void AbstractActivity::setTag(const tag& T) { t = T; }
+
+
+
+
